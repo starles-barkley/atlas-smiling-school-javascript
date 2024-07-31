e85090aa040dfd0d51fff6913804d2c0fb340433
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-07-31 20:31:13 +0000

    create script for carousel

---
+++ homepage.js
@@ -0,0 +1,5 @@
+$(document).ready(() => {
+  quoteCarouselInitializer();
+  videoCarouselInitializer("popular-tutorials", "popular-carousel", "tutorial-loader");
+  videoCarouselInitializer("latest-videos", "latest-carousel", "latest-loader");
+})
